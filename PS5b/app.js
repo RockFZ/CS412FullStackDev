96dc50cb61ad11e94490ce45234f63525340ecc7
Otitodirichukwu <oodarluz@syr.edu>
2020-06-23 14:47:51 -0400

    PS%b comple

--- PS5b/app.js
+++ PS5b/app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 app.use('/ps4', ps4Router);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- PS5b/routes/ps4.js
+++ PS5b/routes/ps4.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 const CONFIG = require('../config/config.js');
+const redis = require('redis');
+const client = redis.createClient();
 
 
 /* GET home page. */
@@ -11,10 +13,38 @@ router.route('/')
         res.render('index', { title: 'Express' });})
 
     .post(async (req, res, next) => {
-        let result = await fetch( CONFIG.url + req.body.number);
-        let tim = await result.json();
-        tim = tim.results;
-        res.render('play',{quest:tim});
+
+            let num = req.body.number;
+            num = String(num);
+
+            client.exists(num, async (err, match) => {
+                if(err) { throw new Error(err) }
+                if (match==1) {
+                        client.get(num, (err, response) => {
+                                console.table(response);
+                                let rep = JSON.parse(response);
+                                rep.cached = "YES";
+                                res.send(JSON.stringify(rep));
+                        })
+                } else {
+                        let result = await fetch( CONFIG.url + req.body.number);
+                        let tim = await result.json();
+                        tim = tim.results;
+                        tim = {
+                                "response": JSON.stringify(tim),
+                                "cached": "No"
+                        }
+                        tim = JSON.stringify(tim);
+                        client.set(num, tim, (err, response) => { //name = key, reversedName = value
+                                console.table(tim);
+                                setTimeout(() => { client.del(num,(err, success) => {
+                                        if (err) { throw new Error(err)}
+                                });}, 30000);
+                                res.send(JSON.stringify(tim));
+                        })
+                }
+            })
+//redis end
     })
 
 // for each object in list grab question correct ans incorrect answers
